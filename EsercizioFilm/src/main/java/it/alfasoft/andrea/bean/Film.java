93c4a66a47ab2609ee4762749f602786de51cd52
and86 <andrea.ln@tiscali.it>
2016-11-10 14:25:06 +0100

    css style

--- EsercizioFilm/src/main/java/it/alfasoft/andrea/bean/Film.java
+++ EsercizioFilm/src/main/java/it/alfasoft/andrea/bean/Film.java
@@ -1,34 +1,50 @@
 package it.alfasoft.andrea.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
-@ManagedBean(name="f", eager=true)
+@ManagedBean(name = "film", eager = true)
 public class Film implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id_film;
 	private String nomeFilm;
-	int data;
+	private int data;
 	private String nomeRegista;
 	private String codiceFilm;
 	private String genere;
-	
-	public Film(){
+	@Transient
+	private List<String> generi;
+
+	public Film() {
+		generi = new ArrayList<String>();
+		generi.add("");
+		generi.add("Thriller");
+		generi.add("Horror");
+		generi.add("Azione");
+		generi.add("Giallo");
+
 	}
-	
+
 	public Film(String nomeFilm, int data, String nomeRegista,
 			String codiceFilm, String genere) {
 		this.nomeFilm = nomeFilm;
@@ -89,9 +105,28 @@ public class Film implements Serializable {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	
-	
-	
-	
+
+	public List<String> getGeneri() {
+		return generi;
+	}
+
+	public void setGeneri(List<String> generi) {
+		this.generi = generi;
+	}
+
+	public void validazioneCodiceFilm(FacesContext context, UIComponent component,
+			Object value) throws ValidatorException {
+
+		if (value == null) {
+			return;
+		}
+		String dato = value.toString();
+		if (!dato.startsWith("Mov")) {
+			FacesMessage msg = new FacesMessage(
+					"Codice Film  errato: deve iniziare con Mov!");
+			throw new ValidatorException(msg);
+		}
+
+	}
 
 }
--- EsercizioFilm/src/main/java/it/alfasoft/andrea/bean/FilmController.java
+++ EsercizioFilm/src/main/java/it/alfasoft/andrea/bean/FilmController.java
@@ -1,27 +1,35 @@
 package it.alfasoft.andrea.bean;
 
 
-import it.alfasoft.andrea.dao.FilmDao;
+
 import it.alfasoft.andrea.servizio.Servizio;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name="fC", eager=true)
+@SessionScoped
 public class FilmController implements Serializable {
 
 	/**
 	 * 
 	 */
+	
+	Servizio s;
+	
 	private static final long serialVersionUID = 1L;
 	
 	private List <Film> listaFilm;
 	
 	public FilmController(){
-		Servizio s=new Servizio();	
-		listaFilm=s.getTuttiFilm();
+		s=new Servizio();	
+		
 	}
 
 	public List<Film> getListaFilm() {
@@ -34,6 +42,39 @@ public class FilmController implements Serializable {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+	
+	public String modificaFilm(long id){
+		Film f=s.leggiFilmConId(id);
+		ExternalContext externalContext=FacesContext.getCurrentInstance()
+										.getExternalContext();
+		Map<String,Object> requestMap=externalContext.getRequestMap();
+		requestMap.put("film", f);	//metto nell'oggetto request Film f e poi lo leggo con #theFilm
+		
+		return "ModificaFilm";
+		
+	}
+	
+	public String aggiornaFilm(Film f){
+		s.aggiornaFilm(f);
+		return "TabellaFilm?faces-redirect=true";
+	}
+	
+	public String eliminaFilm(long id){
+		Film f=s.leggiFilmConId(id);
+		s.eliminaFilm(f);
+		return "TabellaFilm?faces-redirect=true";
+	}
+	
+	public String registraFilm(Film f){
+		s.registraFilm(f);
+		
+		return "TabellaFilm?faces-redirect=true";
+	}
+	
+	public void caricaFilm(){
+		listaFilm=s.getTuttiFilm();
+		
 	}
 	
 	
